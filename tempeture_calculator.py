10b10370f3b4357e2ba492b8ebcc4d2029b5b682
braindeddd <63864153+braindeddd@users.noreply.github.com>
2022-09-12 08:38:38 -0500

    Add files via upload

---
+++ tempeture_calculator.py
@@ -0,0 +1,10 @@
+#tempeture converter
+temp = 0
+
+print ("hello welcome to my calculator, basically you put in what you want to calculate ")
+temp = input ("what is the number that you would like me to put into celcius")
+
+final = (int(temp) - 32)
+final = (int(final) * .5556)
+
+print ("your starting number in feiregnhight was ", temp ," and what that is in celcius is ", final)
